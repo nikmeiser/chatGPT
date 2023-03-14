87bfd3701d602ff4345984442a786a036b8ea57a
nikmeiser <nikmeiser@yahoo.com>
2023-03-14 03:26:07 +0200

    first chatgpt experiment

---
+++ ash.py
@@ -0,0 +1,24 @@
+
+import os
+import openai
+
+# apply the API key
+openai.api_key = os.environ["OPENAI_API_KEY"]
+
+# input text
+#text_prompt = str(input('Tell me what you want: '))
+text_prompt = (
+        "How to find the count of each word in a file using standard bash utilities on Linux or UNIX."
+        )
+text_prompt = text_prompt.strip()
+
+response = openai.Completion.create(
+        engine="davinci-codex",
+        prompt= text_prompt,
+        max_tokens=1024,
+        n=1,
+        stop=None,
+        temperature=0.5,
+        )
+
+print(response.choices[0].text.strip())
